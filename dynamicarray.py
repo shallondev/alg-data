6b13c31e0d7d1d3312f686a58dd0b93412e445ed
shallondev <shallon.frances.pawlickpotts@gmail.com>
2024-03-17 11:23:28 -0400

    test

---
+++ dynamicarray.py
@@ -0,0 +1,13 @@
+"""
+Static Array: Items stored in continous memeory
+
+Problem: Once we reach a point in memory where there is something else being stored there, the array can not be increased in size. (Reached capacity)
+"""
+
+"""
+Dynamic Array
+
+When the array reaches capacity a new array is created with a bigger capacity in memeory copying the data over
+
+Problems: Wastes memory if space is not needed.
+"""
